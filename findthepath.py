58decb0ed2e880b963f64d9f4c7255fe2b559703
Hacker-Kid <49053877+Hacker-Kid@users.noreply.github.com>
2019-10-27 12:29:29 +0800

    Add files via upload

---
+++ findthepath.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+import requests
+import threading
+import sys
+import signal
+##python test.py -u http://127.0.0.1/find -t 3 -o 10 -d 1.txt -s save2.txt
+def _init_(banner):
+
+    path_list = ['-u','','-t','','-s','','-d','','-o','']
+    a = sys.argv[1]
+    b = sys.argv[3]
+    c = sys.argv[5]
+    d = sys.argv[7]
+    l = sys.argv[9]
+    if a in path_list:
+        e = path_list.index(a)+1
+        path_list[e]=sys.argv[2]
+    else:
+        print(banner)
+        sys.exit(0)
+    if b in path_list:
+        f = path_list.index(b)+1
+        path_list[f]=sys.argv[4]
+    else:
+        print(banner)
+        sys.exit(0)
+    if c in path_list:
+        g = path_list.index(c)+1
+        path_list[g]=sys.argv[6]
+    else:
+        print(banner)
+        sys.exit(0)
+    if d in path_list:
+        h = path_list.index(d)+1
+        path_list[h]=sys.argv[8]
+    else:
+        print(banner)
+        sys.exit(0)
+    if l in path_list:
+        j = path_list.index(l)+1
+        path_list[j]=sys.argv[10]
+    else:
+        print(banner)
+        sys.exit(0)
+
+    url = path_list[1]#网址
+    thrd = int(path_list[3])#线程数
+    global save
+    save = open(path_list[5],'a')#查询到的地址保存到文件的路径
+    dict_file = path_list[7]#字典的路径
+    over_time = int(path_list[9])#超时时间
+    return url,thrd,save,dict_file,over_time
+
+def _dict(dict_file):
+    file = open(dict_file,'r')
+    files = file.readlines()
+    num = len(files)#字典中fuzz的数量
+    return num,files
+def _send_(files,thred,i,url,save,over_time):
+    global flag
+    flag = 0#丢包次数
+    for admin in files[i*thred:(i+1)*thred]:
+        try:
+            admin = admin.strip('\n')
+            new_url = url+'/'+admin
+            s = requests.get(url=new_url,timeout=over_time)
+            if s.status_code != 404:
+                print('find address --',admin,'| stadus -- ',s.status_code)
+                save.write(admin+'\n')
+                '''
+                如果不是404则将其写入save文件
+                '''
+        except KeyboardInterrupt:
+            break
+        except:
+            flag += 1
+            if flag % 10000==0:
+                print('Already lose',flag,'packets')
+                #每丢10000个包提醒一次
+
+def _send():
+    banner = '''
+    --------------------------------------
+    |       Please input:                |
+    |       -u url                       | 
+    |       -t thread count              |
+    |       -s save to file              |
+    |       -d catalog dictionary        |
+    |       -o timeout to request        |
+    --------------------------------------
+            '''
+    try:
+        url,thrd,save,dict_file,over_time = _init_(banner)
+    except:
+        print(banner)
+        sys.exit(0)
+    #获取初始数据,若缺少则返回banner并退出
+    num,files = _dict(dict_file)#使用文件,此处调用没太大意义
+    thread_list = []
+    if num % thrd==0:
+        thred = num//thrd
+    else:
+        thred = num//thrd
+        thrd += 1
+    #每个线程要尝试的fuzz次数,非整除则加一
+    print('''
+    ___     _                _    _____   _                 ___            _      _      
+   | __|   (_)    _ _     __| |  |_   _| | |_      ___     | _ \  __ _    | |_   | |_    
+   | _|    | |   | ' \   / _` |    | |   | ' \    / -_)    |  _/ / _` |   |  _|  | ' \   
+  _|_|_   _|_|_  |_||_|  \__,_|   _|_|_  |_||_|   \___|   _|_|_  \__,_|   _\__|  |_||_|  
+_| """ |_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_| """ |_|"""""|_|"""""|_|"""""| 
+"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-' 
+
+========================================================================================
+    ''')
+    for i in range(0,thrd):
+        t = threading.Thread(target=_send_,args=(files,thred,i,url,save,over_time))
+        thread_list.append(t)
+        t.start()
+        #开启线程
+    for t in thread_list:
+        t.join()
+        #等待线程跑完
+    print("------------------------------------------Down--------------------------------------------------")
+    #结束
+    save.close()
+
+if __name__ == '__main__':
+    _send()
